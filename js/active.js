00d96722fe9123ca6638e13a9daa705537a9494b
wwwsbd22 <mahfuzhvf402@gmail.com>
2018-02-26 23:49:35 -0800

    class-3

---
+++ js/active.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	
+	
+$('.owl-carousel').owlCarousel({
+    loop:true,
+  
+    nav:true,
+	 navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+	items:1,
+	smartSpeed:1500,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+  
+    nav:true,
+    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+	items:1,
+	smartSpeed:1500, 
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+});
